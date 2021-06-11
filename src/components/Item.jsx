f5490d441183ba7eea5f326ef05ede711c8b4f70
gudbrant <61345734+gudbrant@users.noreply.github.com>
2021-06-11 09:37:55 +0800

    Create Item.jsx

---
+++ src/components/Item.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Item = ({ item }) => {
+  const getTitle = title => {
+    return title.length > 50 ? `${title.substring(0, 50)}…` : title;
+  };
+
+  const getPrice = (price, currency) => {
+    if (currency === "USD") {
+      return `$${price}`;
+    } else if (currency === "EUR") {
+      return `€${price}`;
+    }
+    return `${price} ${currency}`;
+  };
+
+  const getQuantity = quantity => {
+    if (quantity <= 10) {
+      return "low";
+    } else if (quantity <= 20) {
+      return "medium";
+    }
+    return "high";
+  };
+
+  const quantityClass = `item-quantity level-${getQuantity(item.quantity)}`;
+
+  return item.state === "active" ? (
+    <div className="item">
+      <div className="item-image">
+        <a href={item.url}>
+          <img src={item.MainImage.url_570xN} alt="item" />
+        </a>
+      </div>
+      <div className="item-details">
+        <p className="item-title">{getTitle(item.title)}</p>
+        <p className="item-price">{getPrice(item.price, item.currency_code)}</p>
+        <p className={quantityClass}>{`${item.quantity} left`}</p>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default Item;
